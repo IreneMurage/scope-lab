ec2a07fc411e78b4e71718db4300d4062644f37e
IreneMurage <muthonimurage361@gmail.com>
2025-06-23 20:56:03 +0300

    put data to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = "not bob"
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = "maybe bob";
+
+}
+
+const leastFavoriteCustomer = "Irene";
+
+function changeLeastFavoriteCustomer(){
+    var leastFavoriteCustomer = "Muthoni";
+}
